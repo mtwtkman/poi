fbb015785472a75d8efc79a5ddd547ab67a6d915
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2022-10-23 10:19:56 +0900

    dry

--- lib/Poi/Operation/UpdateMetaInfo.hs
+++ lib/Poi/Operation/UpdateMetaInfo.hs
@@ -28,15 +28,33 @@ getCurrentWholeMetaInfo trashbox = do
         Left DeserializeFailed -> return (Left FileNotFound)
     else return (Left FileNotFound)
 
+currentWholeMetaInfoContext :: TrashBox -> MetaInfo -> ([MetaInfo] -> MetaInfo -> a) -> IO (MetaInfoFileAccessResult a)
+currentWholeMetaInfoContext tb m process = do
+  contents <- getCurrentWholeMetaInfo tb
+  case contents of
+    Right metas -> do
+      return $ Right (process metas m)
+    Left _ -> return (Left FileNotFound)
+
+
 addMetaInfo :: [MetaInfo] -> MetaInfo -> [MetaInfo]
 addMetaInfo metas = (<>) metas . pure
 
 writeMetaInfoFile :: TrashBox -> [MetaInfo] -> IO ()
 writeMetaInfoFile tb metas = writeFile (metaInfoFileLocation tb) (unlines $ map serialize metas)
 
+updateMetaInfoFile :: TrashBox -> MetaInfo -> ([MetaInfo] -> MetaInfo -> MetaInfoUpdateResult a) -> IO (MetaInfoFileAccessResult a)
+updateMetaInfoFile tb m process = do
+  contents <- getCurrentWholeMetaInfo tb
+  case contents of
+    Right metas -> do
+      case process metas m of
+        Right result ->  return $ Right result
+        Left reason -> return $ Left (MetaInfoFileUpdateError reason)
+    Left _ -> return $ Left FileNotFound
 
-updateMetaInfoFile :: TrashBox -> MetaInfo -> IO (MetaInfoFileAccessResult [MetaInfo])
-updateMetaInfoFile tb m = do
+addMetaInfoToFile :: TrashBox -> MetaInfo -> IO (MetaInfoFileAccessResult [MetaInfo])
+addMetaInfoToFile tb m = do
   contents <- getCurrentWholeMetaInfo tb
   case contents of
     Right metas -> do
