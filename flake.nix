fb5536f8047c4b188d109c7a0109e8b4fbf150a2
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2022-10-30 11:16:26 +0900

    bash-prompt

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
+  nixConfig.bash-prompt = "\\e[0;32m\\u@\\w$ \\e[m";
   outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem
       (system:
