b544896edcca41c2a8f06136b5bc4950922579f1
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2022-10-30 16:58:11 +0900

    added prompt

--- lib/Poi/Action/Prompt.hs
+++ lib/Poi/Action/Prompt.hs
@@ -1,1 +1,33 @@
 module Poi.Action.Prompt where
+
+import Control.Monad
+import Data.Either
+import Poi.Action.List
+import Poi.Action.UpdateMetaInfo
+import Poi.Entity
+import Text.Read
+
+validIndex :: Int -> [MetaInfo] -> Bool
+validIndex i ms = i >= 0 && length ms > i
+
+askMetaInfoIndex :: TrashBox -> IO Int
+askMetaInfoIndex tb = do
+  printCurrentIndexedMetaInfoList tb
+  msResult <- getCurrentWholeMetaInfo tb
+  when (isLeft msResult) (fail "TrashBox is empty")
+  let (Right ms) = msResult
+  question tb ms
+  where
+    question tb ms = do
+      putStr "Select number to manipulate >"
+      input <- getLine
+      case readMaybe input of
+        Nothing -> do
+          print "Please input the value from index number"
+          question tb ms
+        Just choosen -> do
+          if validIndex choosen ms
+            then return choosen
+            else do
+              print "Please input the value from index number"
+              question tb ms
