5125bffeef0f42406a1d3c72556422713bfdff74
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2024-02-20 20:25:36 +0900

    improved Reader monad

--- poi-core/Poi/Control/Monad/Trans/Reader.hs
+++ poi-core/Poi/Control/Monad/Trans/Reader.hs
@@ -1,14 +1,24 @@
+{-# LANGUAGE KindSignatures #-}
+
 module Poi.Control.Monad.Trans.Reader where
 
+import Data.Functor.Identity (Identity (runIdentity))
+import Data.Kind (Type)
 import Poi.Control.Monad.Free (Free (..))
 
-type ReaderF r = (->) r
+newtype ReaderT r (m :: Type -> Type) a = ReaderT {runReaderT :: r -> m a}
 
-type Reader r = Free (ReaderF r)
+instance (Functor m) => Functor (ReaderT r m) where
+  fmap f (ReaderT rf) = ReaderT $ \r -> fmap f (rf r)
+
+type Reader r = Free (ReaderT r Identity)
 
 ask :: Reader r r
-ask = Impure Pure
+ask = reader id
+
+reader :: (r -> a) -> Reader r a
+reader f = f <$> ask
 
 runReader :: Reader r a -> r -> a
 runReader (Pure a) _ = a
-runReader (Impure k) r = runReader (k r) r
+runReader (Impure k) r = runReader (runIdentity $ runReaderT k r) r
